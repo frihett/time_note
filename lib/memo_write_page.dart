7d5e59e02ac75ae11a88906daeb45a769b2331fc
sseung <sseutta@naver.com>
2024-12-05 13:00:36 +0900

    feat: 메모 작성 페이지 작성

---
+++ lib/memo_write_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MemoWritePage extends StatefulWidget {
+  @override
+  _MemoWritePageState createState() {
+    return _MemoWritePageState();
+  }
+}
+
+class _MemoWritePageState extends State<MemoWritePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
