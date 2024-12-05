81ce0368d6f17c5a4c4e86ab3ec0bb7610f4ca33
sseung <sseutta@naver.com>
2024-12-05 13:00:43 +0900

    feat: 타임 세팅 페이지 작성

---
+++ lib/time_setting_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TimeSettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('타임 셋팅'),
+      ),
+      body: Center(),
+    );
+  }
+}
