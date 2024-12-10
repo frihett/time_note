d1255d15e40a722b7d5915fc5a6f66a3a34efa90
sseung <sseutta@naver.com>
2024-12-10 15:28:29 +0900

    feat: 홈페이지 UI 변경, 토글 추가

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:time_note/config/ui_style/UiStyle.dart';
 import 'package:time_note/model/time_setting.dart';
 import 'package:time_note/presentation/home/home_page_view_model.dart';
 
@@ -25,21 +26,52 @@ class _HomePageState extends State<HomePage> {
             );
           }
           return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+            padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
             child: ListView.builder(
               itemCount: viewModel.timeSettings.length,
               itemBuilder: (context, index) {
                 final timeSetting = viewModel.timeSettings[index];
-                return ListTile(
-                  title: Text(
-                    '${timeSetting.period} ${timeSetting.hour}:${timeSetting.minute.toString().padLeft(2, '0')}',
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      viewModel.deleteTimeSetting(timeSetting.id!);
-                    },
-                  ),
+                return GestureDetector(
+                  onLongPress: () {
+                    viewModel.deleteTimeSetting(timeSetting.id!);
+                  },
+                  child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 12.h),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 28.h, horizontal: 20.w),
+                      decoration: BoxDecoration(
+                          color: UiStyle.secondaryColorSurface,
+                          borderRadius: BorderRadius.circular(12.r)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${timeSetting.period} ${timeSetting.hour}:${timeSetting.minute.toString().padLeft(2, '0')}',
+                            style: UiStyle.h3Style
+                                .copyWith(color: UiStyle.color[700]),
+                          ),
+                          Transform.scale(
+                            scale: 0.8,
+                            child: Switch(
+                              activeColor: Colors.white,
+                              activeTrackColor: UiStyle.secondaryColor,
+                              inactiveThumbColor: Colors.white,
+                              trackOutlineWidth: WidgetStateProperty.all(0.0),
+                              trackOutlineColor:
+                                  WidgetStateProperty.all(Colors.transparent),
+                              thumbIcon: WidgetStateProperty.all(
+                                Icon(Icons.circle,
+                                    size: 20, color: Colors.white),
+                              ),
+                              value: timeSetting.isToggled,
+                              onChanged: (value) {
+                                viewModel.updateToggleState(
+                                    timeSetting.id!, value);
+                              },
+                            ),
+                          )
+                        ],
+                      )),
                 );
               },
             ),
@@ -49,7 +81,6 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final result = await context.push('/timeSettingPage');
-
 
           if (result != null && result is Map<String, dynamic>) {
             final newTimeSetting = TimeSetting(
