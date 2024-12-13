14e6dfe8c6893960f2a3ae4f660171d2322212c0
sseung <sseutta@naver.com>
2024-12-13 15:09:02 +0900

    feat: 한국어 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:time_note/config/ui_style/UiStyle.dart';
+import 'package:time_note/data_source/memo_database_service.dart';
 import 'package:time_note/data_source/time_settings_database_service.dart';
 import 'package:time_note/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ko_KR', '');
   await TimeSettingsDatabaseService.instance.database;
-
+  await MemoDatabaseService.instance.database;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
