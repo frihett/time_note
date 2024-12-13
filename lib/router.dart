ed222955beaf62f79c53ac309185544d8c7ff8a2
sseung <sseutta@naver.com>
2024-12-13 15:12:33 +0900

    feat: 메모 관련 페이지 의존성 주입, 인자 변경

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,16 @@
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:time_note/data_source/memo_database_service.dart';
 import 'package:time_note/data_source/time_settings_database_service.dart';
 import 'package:time_note/main_page.dart';
-import 'package:time_note/memo_manage_page.dart';
-import 'package:time_note/memo_write_page.dart';
+import 'package:time_note/presentation/memo/memo_manage_page.dart';
+import 'package:time_note/presentation/memo/memo_manage_view_model.dart';
+import 'package:time_note/presentation/memo/memo_write_page.dart';
 import 'package:time_note/presentation/home/home_page.dart';
 import 'package:time_note/presentation/home/home_page_view_model.dart';
-import 'package:time_note/repository/time_settings_repository_local.dart';
+import 'package:time_note/presentation/memo/memo_write_view_model.dart';
+import 'package:time_note/repository/memo/memo_repository_local.dart';
+import 'package:time_note/repository/time_setting/time_settings_repository_local.dart';
 import 'package:time_note/time_setting_page.dart';
 
 final GoRouter goRouter = GoRouter(
@@ -32,7 +36,12 @@ final GoRouter goRouter = GoRouter(
         GoRoute(
           path: '/memo',
           builder: (context, state) {
-            return MemoManagePage();
+            return ChangeNotifierProvider(
+              create: (context) => MemoManageViewModel(
+                  memoRepository: MemoRepositoryLocal(
+                      databaseService: MemoDatabaseService.instance)),
+              child: MemoManagePage(),
+            );
           },
         ),
       ],
@@ -46,7 +55,26 @@ final GoRouter goRouter = GoRouter(
     GoRoute(
       path: '/memoWritePage',
       builder: (context, state) {
-        return MemoWritePage(); // MainPage 외부의 페이지
+        final extra = state.extra as Map<String, dynamic>;
+        final period = extra['period'] as String;
+        final hour = extra['hour'] as int;
+        final minute = extra['minute'] as int;
+        final year = extra['year'] as int;
+        final month = extra['month'] as int;
+        final day = extra['day'] as int;
+        return ChangeNotifierProvider(
+          create: (context) => MemoWriteViewModel(
+            period: period,
+            hour: hour,
+            minute: minute,
+            year: year,
+            month: month,
+            day: day,
+            memoRepository: MemoRepositoryLocal(
+                databaseService: MemoDatabaseService.instance),
+          ),
+          child: MemoWritePage(),
+        );
       },
     ),
   ],
